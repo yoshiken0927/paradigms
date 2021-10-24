61b2b94d62c43144573c482d398aa23540f6a4ac
yoshiken0927 <51016793+yoshiken0927@users.noreply.github.com>
2021-10-24 15:02:25 +0900

    Update main.hs

--- functional/main.hs
+++ functional/main.hs
@@ -1,15 +1,16 @@
-
 data Employee = Employee { name::String, age::Int }
 
 findEmployee :: String -> Employee
 setAge :: Employee -> Int -> Employee
 updateEmployee :: String -> Int -> Employee
+updateEmployee2 :: Employee -> Employee
 
 findEmployee name = Employee { name = name, age = 30 }
+updateEmployee2 e = e
+
 setAge employee age = employee { age = age }
-updateEmployee name age = setAge (findEmployee name) age
 
-str = "xyz"
-someFunc3 = take 1 str
+--updateEmployee name = setAge (findEmployee name)
+updateEmployee name age = updateEmployee2(setAge (findEmployee name) age)
 
-print = putStrLn(name (findEmployee "yoshiken") ++ ":" ++ show (age (updateEmployee "yoshiken" 31)))
+someFunc = putStrLn(name (findEmployee "yoshiken") ++ ":" ++ show (age (updateEmployee "yoshiken" 31)))
